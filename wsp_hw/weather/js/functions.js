79e19becda19d3fdf1ba87dc01bcf54d259aa844
marencamille19 <ste18009@byui.edu>
2019-02-27 10:02:28 -0700

    changes

--- wsp_hw/weather/js/functions.js
+++ wsp_hw/weather/js/functions.js
@@ -1,13 +1,17 @@
-getCondition() {
+/* *************************************
+ *  Weather Site JavaScript Functions
+ ************************************* */
+//console.log('My javascript is being read.');
 
+
+//calculate the Windchill
+function buildWC(windSpeed, currentTemp) {
+    const feelTemp = document.getElementById('feels-like');
+    // Compute the windchill
+    let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
+    wc = (wc > temp)?temp:wc;
+    wc = Math.floor(wc);
+    console.log(wc);
+    wc = 'Feels like ' + wc + '&deg;F';
+    feels-like.innerHTML = wc;
 }
-    
-changeSummaryImage(){
-
-}
-
-convertMeters(){
-
-}
-
-
