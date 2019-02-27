b7375c855991187809b9ae12b500a07ae0770002
marencamille19 <ste18009@byui.edu>
2019-02-27 10:42:00 -0700

    wind pointer finished js

--- wsp_hw/weather/js/functions.js
+++ wsp_hw/weather/js/functions.js
@@ -7,6 +7,9 @@ const speed = 5;
 
 buildWC(speed, temp);
 
+const direction = "NNE";
+
+windDial(direction);
 //calculate the Windchill
 function buildWC(speed, temp) {
     const feelTemp = document.getElementById('feels-like');
@@ -20,4 +23,49 @@ function buildWC(speed, temp) {
     feelsLike.innerHTML = wc;
 
     console.log(wc);
+}
+
+// Wind Dial Function
+function windDial(direction){
+    const dial = document.getElementById("dial");
+        console.log(direction);
+        // Determine the dial class
+        switch (direction){
+         case "North":
+         case "N":
+          dial.setAttribute("class", "n"); //"n" is the CSS rule selector
+          break;
+         case "NE":
+         case "NNE":
+         case "ENE":
+          dial.setAttribute("class", "ne");
+          break;
+         case "NW":
+         case "NNW":
+         case "WNW":
+          dial.setAttribute("class", "nw");
+          break;
+         case "South":
+         case "S":
+          dial.setAttribute("class", "s");
+          break;
+         case "SE":
+         case "SSE":
+         case "ESE":
+          dial.setAttribute("class", "se");
+          break;
+         case "SW":
+         case "SSW":
+         case "WSW":
+          dial.setAttribute("class", "sw");
+          break;
+         case "East":
+         case "E":
+          dial.setAttribute("class", "e");
+          break;
+         case "West":
+         case "W":
+          dial.setAttribute("class", "w");
+          break;
+        }
 }
