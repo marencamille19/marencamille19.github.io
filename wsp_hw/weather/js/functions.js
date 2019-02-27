bb416b3e937f2129fa4369a6fd9cecfc92bcfc68
marencamille19 <ste18009@byui.edu>
2019-02-27 10:27:33 -0700

    wind pointer css

--- wsp_hw/weather/js/functions.js
+++ wsp_hw/weather/js/functions.js
@@ -2,16 +2,22 @@
  *  Weather Site JavaScript Functions
  ************************************* */
 //console.log('My javascript is being read.');
+const temp = 31;
+const speed = 5;
 
+buildWC(speed, temp);
 
 //calculate the Windchill
-function buildWC(windSpeed, currentTemp) {
+function buildWC(speed, temp) {
     const feelTemp = document.getElementById('feels-like');
     // Compute the windchill
     let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
+    console.log(wc);
     wc = (wc > temp)?temp:wc;
     wc = Math.floor(wc);
-    console.log(wc);
+
     wc = 'Feels like ' + wc + '&deg;F';
-    feels-like.innerHTML = wc;
+    feelsLike.innerHTML = wc;
+
+    console.log(wc);
 }
