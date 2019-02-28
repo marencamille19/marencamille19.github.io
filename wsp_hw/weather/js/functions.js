1f5a073b501cf761a0f04b7d25fee9cde47b1475
marencamille19 <ste18009@byui.edu>
2019-02-27 18:36:18 -0700

    deleted unused things

--- wsp_hw/weather/js/functions.js
+++ wsp_hw/weather/js/functions.js
@@ -17,7 +17,7 @@ getCondition(weather);
 //calculate the Windchill
 function buildWC(speed, temp) {
     const feelsLike = document.getElementById('feelsLike');
-    // Compute the windchill
+     // Compute the windchill
     let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
     console.log(wc);
 
@@ -29,26 +29,6 @@ function buildWC(speed, temp) {
     wc = 'Feels like ' + wc + '&deg;F';
     feelsLike.innerHTML = wc;
 }
-
-//  // Calculate the Windchill
-//  function buildWC(speed, temp) {
-//     const feelTemp = document.getElementById('feelTemp');
-   
-//     // Compute the windchill
-//     let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
-//     console.log(wc);
-   
-//     // Round the answer down to integer
-//     wc = Math.floor(wc);
-   
-//     // If chill is greater than temp, return the temp
-//     wc = (wc > temp)?temp:wc;
-   
-//     // Display the windchill
-//     console.log(wc);
-//     // wc = 'Feels like '+wc+'°F';
-//     feelTemp.innerHTML = wc;
-//     }
 
 // Wind Dial Function
 function windDial(direction) {
@@ -137,7 +117,7 @@ function getCondition(phrase) {
 }
 
 function changeSummaryImage(weather) {
-
+    
 }
 
 function convertMeters(meters) {
