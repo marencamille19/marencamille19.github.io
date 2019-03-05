b9b0356ea240c7c88f974499b048d6092ec52e4a
marencamille19 <ste18009@byui.edu>
2019-03-05 12:23:19 -0700

    edits

---
+++ wsp_hw/weather/js/localdata.js
@@ -0,0 +1,9 @@
+
+
+
+
+
+
+let pageNav = document.getElementById('page-nav');
+let statusContainer = document.getElementById('status');
+let contentContainer = document.getElementById('page-content');
