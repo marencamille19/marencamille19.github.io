90d51505bdeb1394da45713649548ba6346aa4b2
marencamille19 <ste18009@byui.edu>
2019-03-15 11:57:18 -0600

    sandbox

--- sandbox/loctest.js
+++ sandbox/loctest.js
@@ -1,4 +1,11 @@
 'use strict';
+
+//Set global variable for custom header required by NWS API
+var idHeader = {
+    headers: {
+        "User-Agent": "Student Learning Project = ste18009@byui.edu"
+    }
+}
 //call the function to get our location
 console.log(`Lat and Long are: ${locale}.`);
 getGeoLocation(locale);
@@ -38,3 +45,31 @@ function getGeoLocation(){
         status.innerHTML = "Your browser doesn't support Geolocation or it is not enabled!";
        } // end else
 }//end getGeolocation
+
+// Gets location information from the NWS API
+function getLocation(locale) {
+    const URL = "https://api.weather.gov/points/" + locale; 
+    // NWS User-Agent header (built above) will be the second parameter 
+    fetch(URL, idHeader) 
+    .then(function(response){
+      if(response.ok){ 
+       return response.json(); 
+      } 
+      throw new ERROR('Response not OK.');
+    })
+    .then(function (data) { 
+      // Let's see what we got back
+      console.log('Json object from getLocation function:'); 
+      console.log(data);
+      // Store data to localstorage 
+      storage.setItem("locName", data.properties.relativeLocation.properties.city); 
+      storage.setItem("locState", data.properties.relativeLocation.properties.state); 
+   
+      // Next, get the weather station ID before requesting current conditions 
+      // URL for station list is in the data object 
+      let stationsURL = data.properties.observationStations; 
+      // Call the function to get the list of weather stations
+      //getStationId(stationsURL); 
+     }) 
+    .catch(error => console.log('There was a getLocation error: ', error)) 
+   } // end getLocation function
